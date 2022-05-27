1497b09cbf2261956de0dbd15c9bfa54cc08f381
jedade <42737500+jedade@users.noreply.github.com>
2022-05-27 12:27:13 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagrams(word, anagram):
+    # [assignment] Add your code here
+    if(sorted(word)!= sorted(anagram)):
+        return False
+    return True
+
+print(find_anagrams("hello", "check"))
